fc2a6c88a6c7008523b1eb7e55f4ee35a4c35f97
Admin <conner.p.mccabe@gmail.com>
2019-12-25 10:53:14 -0600

    add username form component

---
+++ src/components/UsernameForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UsernameForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+    }
+  }
+
+
+}
+
+export default UsernameForm;
