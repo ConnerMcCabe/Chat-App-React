7bbf7fe4ef73e67b5c4669242fb09c87948cc0cf
Admin <conner.p.mccabe@gmail.com>
2019-12-30 13:30:11 -0600

    relearning dotenv

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const ChatKit = require('@pusher/chatkit-server')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ChatKit = require('@pusher/chatkit-server');
 
-const app = express()
+require('custom-env').env();
+const app = express();
 
 const chatkit = new ChatKit.default({
-  instanceLocator: 'coming soon',
-  key: 'coming soon',
-})
+  instanceLocator: 'DB_INSTANCE',
+  key: 'DB_KEY',
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+console.log(process.env.TEST)
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/users', (req, res) => {
   const { username } = req.body
@@ -29,18 +32,18 @@ app.post('/users', (req, res) => {
         res.status(error.status).json(error)
       }
     })
-})
+});
 
 app.post('/authenticate', (req, res) => {
   const authData = chatkit.authenticate({ userId: req.query.user_id })
   res.status(authData.status).send(authData.body)
-})
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, err => {
   if (err) {
     console.error(err)
   } else {
     console.log(`Running on port ${PORT}`)
   }
-})
+});
